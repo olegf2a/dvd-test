788e4735a88dbc11b3872644b0e4e2fd76cba7d8
Oleh Velychko <ovelychko@binaryanvil.com>
2018-04-22 20:43:27 +0300

    Divided views to groups with common responsibilities.

---
+++ rental_department/views/dvd.py
@@ -0,0 +1,28 @@
+from rental_department.models import Dvd
+from rental_department.filters import DvdFilter
+from django.views import generic
+from django.shortcuts import render
+
+
+def index(request):
+    num_dvd = Dvd.objects.all().count()
+    return render(
+        request,
+        'index.html',
+        context={'num_dvd': num_dvd, },
+    )
+
+
+class DvdDetailView(generic.DetailView):
+    model = Dvd
+
+
+class DvdListView(generic.ListView):
+    model = Dvd
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        ctx = super(DvdListView, self).get_context_data(**kwargs)
+        ctx['filter'] = DvdFilter()
+        return ctx
+
