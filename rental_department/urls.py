fb17aab146278a45d0701dbb6ed104a5d13ae04e
Oleh Velychko <ovelychko@binaryanvil.com>
2018-04-26 16:30:29 +0300

    Added namespace to rental urls.

--- rental_department/urls.py
+++ rental_department/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'rental'
+
 urlpatterns = [
     path('', views.index, name='rental'),
-    path('dvd-list/', views.DvdListView.as_view(), name='rental-dvd-list'),
-    path('dvd/<int:pk>', views.DvdDetailView.as_view(), name='rental-dvd-detail'),
+    path('dvd-list/', views.DvdListView.as_view(), name='dvd-list'),
+    path('dvd/<int:pk>', views.DvdDetailView.as_view(), name='dvd-detail'),
     path('search', views.search, name='search'),
     path('rent/<int:pk>', views.rent_dvd, name='rent-dvd'),
     path('return/<int:pk>', views.return_dvd, name='return-dvd'),
